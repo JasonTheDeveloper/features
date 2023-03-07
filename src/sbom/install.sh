f22af475922eb36671a1888b482f853a8554673b
Jason <jagoodse@microsoft.com>
2023-03-08 09:52:14 +1100

    fix(sbom): remove download binary in installation step

--- src/sbom/install.sh
+++ src/sbom/install.sh
@@ -38,7 +38,7 @@ validate_version_exists() {
 }
 
 # make sure we have curl
-check_packages curl jq
+check_packages curl jq ca-certificates libicu-dev
 
 # make sure version is available
 if [ "${SBOM_TOOL_VERSION}" = "latest" ]; then SBOM_TOOL_VERSION=$(curl -sL https://api.github.com/repos/microsoft/sbom-tool/releases/latest | jq -r ".tag_name"); fi
@@ -52,7 +52,7 @@ url="https://github.com/microsoft/sbom-tool/releases/download/${SBOM_TOOL_VERSIO
 echo "Downloading ${url}..."
 curl -sSL https://github.com/microsoft/sbom-tool/releases/download/"${SBOM_TOOL_VERSION}"/"${SBOM_FILENAME}" -o "${SBOM_COMMAND}"
 install -m 555 ${SBOM_COMMAND} /usr/local/bin/${SBOM_COMMAND}
-rm "${SBOM_FILENAME}"
+rm "${SBOM_COMMAND}"
 
 # Clean up
 rm -rf /var/lib/apt/lists/*
